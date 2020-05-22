f2bb2ccd4a7975ef5e9862abeec81a6599480095
FrnkDarko <65276414+FrnkDarko@users.noreply.github.com>
2020-05-22 16:24:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <cassert>
@@ -6,32 +5,35 @@
 double constexpr beta = 0; //probably not the best way to do this
 double constexpr gamma = 0;
 
-struct State {
+struct State
+{
   double S;
   double I;
   double R;
 };
 
-class Epidemic {
+class Epidemic
+{
   State s0_;
   int const N_;
 
- public:
-  Epidemic(State const& s0, int N) : s0_{s0}, N_{N} {}
-  std::vector<State> evolve() {
+public:
+  Epidemic(State const &s0, int N) : s0_{s0}, N_{N} {}
+  std::vector<State> evolve()
+  {
     std::vector<State> states{s0_};
-    int period; //don't know if it should be here or what
-    for (int i = 0; i != period; ++i) {
-        State s{};
-        State const& prev = states.back();
-        double S = prev.S - beta * prev.I * prev.S + 0.5;
-        double I = prev.I + beta * prev.I * prev.S - gamma * prev.I + 0.5;
-        double R = prev.R + gamma * prev.I + 0.5;
-        s.S = static_cast<int>(S);
-        s.I = static_cast<int>(I);
-        s.R = static_cast<int>(R);
-        assert(N_ == S + I + R);
-        states.push_back(s);
+    while (s0_.S != 0)
+    {
+      State s{};
+      State const &prev = states.back();
+      double S = prev.S - beta * prev.I * prev.S + 0.5;
+      double I = prev.I + beta * prev.I * prev.S - gamma * prev.I + 0.5;
+      double R = prev.R + gamma * prev.I + 0.5;
+      s.S = static_cast<int>(S);
+      s.I = static_cast<int>(I);
+      s.R = static_cast<int>(R);
+      assert(N_ == S + I + R);
+      states.push_back(s);
     };
     return states;
   };
@@ -39,7 +41,8 @@ class Epidemic {
 
 void print(){};
 
-int main() {
+int main()
+{
   State s0{};
   int N;
   s0.S = N - 1;
