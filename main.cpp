309ce0adbf96d97890e2ed95742b65323f8c2149
FrnkDarko <65276414+FrnkDarko@users.noreply.github.com>
2020-05-22 19:01:26 +0200

    Aggiunta print, problemi con evolve
    Print dovrebbe funzionare. Ho aggiunto valori di beta, gamma e N per testarlo.
    Evolve non funziona, le equazioni restituiscono risultati impossibili. Non ho risolto il problema.
    Ho tentato cambiando alcune cose, sempre dentro evolve, prendendo ispirazione da Orbit, ma non funziona in ogni caso

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <cassert>
 
-double constexpr beta = 0; //probably not the best way to do this
-double constexpr gamma = 0;
+double constexpr beta = 0.1; 
+double constexpr gamma = 0.5;
 
 struct State
 {
@@ -18,17 +18,27 @@ class Epidemic
   int const N_;
 
 public:
-  Epidemic(State const &s0, int N) : s0_{s0}, N_{N} {}
-  std::vector<State> evolve()
+  Epidemic(State const& s0, int N) : s0_{s0}, N_{N} {}
+  std::vector<State> evolve() const
   {
-    std::vector<State> states{s0_};
-    while (s0_.S != 0)
+    std::vector<State> states; 
+    states.push_back(s0_);
+    auto prev = s0_;
+    while (s0_.I != 0) //ho messo gli infetti, perché se sono 0 finisce per forza l'epidemia
     {
-      State s{};
-      State const &prev = states.back();
-      double S = prev.S - beta * prev.I * prev.S + 0.5;
-      double I = prev.I + beta * prev.I * prev.S - gamma * prev.I + 0.5;
-      double R = prev.R + gamma * prev.I + 0.5;
+      State s;
+      double S = prev.S - (beta * prev.I * prev.S) + 0.5;
+      if (S < 0){
+        S = 0;
+      }
+      double I = prev.I + (beta * prev.I * prev.S) - (gamma * prev.I) + 0.5;
+      if (I > N_){
+        I = N_;
+      }
+      double R = prev.R + (gamma * prev.I);
+      if (R > N_){
+        R = N_;
+      }
       s.S = static_cast<int>(S);
       s.I = static_cast<int>(I);
       s.R = static_cast<int>(R);
@@ -39,15 +49,24 @@ public:
   };
 };
 
-void print(){};
+void print(std::vector<State> const &st)
+{
+  for (auto const &state : st)
+  {
+    std::cout << "S = " << state.S << '\n';
+    std::cout << "I = " << state.I << '\n';
+    std::cout << "R = " << state.R << '\n';
+    std::cout << "Ro = " << state.S * beta / gamma << '\n';
+  }
+};
 
 int main()
 {
   State s0{};
-  int N;
+  int N = 100; //popolazione prova
   s0.S = N - 1;
   s0.I = 1;
   Epidemic e{s0, N};
-  e.evolve();
-  print();
+  auto spread = e.evolve();
+  print(spread);
 }
