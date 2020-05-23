51814af9ea18a7bec81658bc05195d3bbf7149e1
siberiankhatru <martinabevini@gmail.com>
2020-05-23 18:03:46 +0200

    Add if/else loop in evolve, minor fixes
    Moved some stuff around in evolve(), changed while condition

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <cassert>
 
-double constexpr beta = 0.1; //probably not the best way to do this
-double constexpr gamma = 0.5;
+double constexpr beta = 0.1; //trial value
+double constexpr gamma = 0.5; //trial value
 
 struct State
 {
@@ -21,14 +21,23 @@ public:
   Epidemic(State const &s0, int N) : s0_{s0}, N_{N} {}
   std::vector<State> evolve()
   {
-    std::vector<State> states{s0_};
-    while (s0_.S != 0)
+    std::vector<State> states{s0_};      
+    State const &prev = states.back();
+    while (prev.R != N_)
     {
       State s{};
-      State const &prev = states.back();
-      double S = prev.S - beta * prev.I * prev.S + 0.5;
-      double I = prev.I + beta * prev.I * prev.S - gamma * prev.I + 0.5;
-      double R = prev.R + gamma * prev.I + 0.5;
+      if (prev.S > 0) 
+      {
+          double S = prev.S - beta * prev.I * prev.S + 0.5;
+          double I = prev.I + beta * prev.I * prev.S - gamma * prev.I + 0.5;
+          double R = prev.R + gamma * prev.I + 0.5;
+      }
+      else 
+      {
+          double S = 0;
+          double I = N_ - R;
+          double R = prev.R + gamma * prev.I + 0.5;
+      }
       s.S = static_cast<int>(S);
       s.I = static_cast<int>(I);
       s.R = static_cast<int>(R);
@@ -46,14 +55,14 @@ void print(std::vector<State> const &st)
     std::cout << "S = " << state.S << '\n';
     std::cout << "I = " << state.I << '\n';
     std::cout << "R = " << state.R << '\n';
-    std::cout << "Ro = " << state.S * beta / gamma << '\n';
+    std::cout << "R_0 = " << state.S * beta / gamma << '\n';
   }
 };
 
 int main()
 {
   State s0{};
-  int N = 100; //popolazione prova
+  int N = 100; //trial value
   s0.S = N - 1;
   s0.I = 1;
   Epidemic e{s0, N};
