0b7c8764baf8cb26eea06a77fd70d256bf651c05
FrnkDarko <65276414+FrnkDarko@users.noreply.github.com>
2020-06-06 17:33:56 +0200

    fixed some declarations in evolve
    It still doesn't work

--- main.cpp
+++ main.cpp
@@ -18,34 +18,41 @@ class Epidemic
   int const N_;
 
 public:
-  Epidemic(State const &s0, int N) : s0_{s0}, N_{N} {}
-  std::vector<State> evolve()
+  Epidemic(State const &s0, int const N) : s0_{s0}, N_{N} {}
+  std::vector<State> evolve() const
   {
-    std::vector<State> states{s0_};      
-    State const &prev = states.back();
+    std::vector<State> states;  
+    states.push_back(s0_);    
+    State prev = states.back();
+    double S;
+    double I;
+    double R;
     while (prev.R != N_)
     {
-      State s{};
+      State s;
       if (prev.S > 0) 
       {
-          double S = prev.S - beta * prev.I * prev.S + 0.5;
-          double I = prev.I + beta * prev.I * prev.S - gamma * prev.I + 0.5;
-          double R = prev.R + gamma * prev.I + 0.5;
+           S = prev.S - (beta * prev.I * prev.S);
+           I = prev.I + (beta * prev.I * prev.S) - (gamma * prev.I);
+           R = prev.R + gamma * prev.I;
       }
       else 
       {
-          double S = 0;
-          double I = N_ - R;
-          double R = prev.R + gamma * prev.I + 0.5;
+           S = 0;
+           R = prev.R + gamma * prev.I;
+           I = N_ - R;
+          
       }
+      double sum = S + I + R;
+      assert(sum == N_);
       s.S = static_cast<int>(S);
       s.I = static_cast<int>(I);
-      s.R = static_cast<int>(R);
-      assert(N_ == S + I + R);
+      s.R = static_cast<int>(R) + 0.5;
       states.push_back(s);
-    };
+      prev = s;
+    }
     return states;
-  };
+  }
 };
 
 void print(std::vector<State> const &st)
@@ -57,15 +64,15 @@ void print(std::vector<State> const &st)
     std::cout << "R = " << state.R << '\n';
     std::cout << "R_0 = " << state.S * beta / gamma << '\n';
   }
-};
+}
 
 int main()
 {
   State s0{};
-  int N = 100; //trial value
+  int const N = 100; //trial value
   s0.S = N - 1;
   s0.I = 1;
-  Epidemic e{s0, N};
+  Epidemic e(s0, N);
   auto spread = e.evolve();
   print(spread);
 }
