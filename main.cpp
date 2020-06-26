42a8cbfb384713da243bfcbfbb1cc7e1713c479e
siberiankhatru <martinabevini@gmail.com>
2020-06-26 17:15:40 +0200

    Add error if gamma==0, add doctest

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
 #include <iostream>
 #include <vector>
 #include <cassert>
@@ -28,11 +31,14 @@ public:
 	}
 	std::vector<State> evolve(double const beta, double const gamma) const
 	{
-		assert(beta >= 0. && beta <= 1.);
+		assert(beta > 0. && beta <= 1.);
 		if (beta == 0.) {
 			throw std::runtime_error{ "no epidemic" };
 		}
-		assert(gamma >= 0. && gamma <= 1.);
+		assert(gamma > 0. && gamma <= 1.);
+		if (gamma == 0) {
+			throw std::runtime_error{ "no chance of recovery" };
+		}
 		std::vector <State> states;
 		states.push_back(s0_);
 		State prev = states.back();
@@ -74,7 +80,7 @@ void print(std::vector<State> const& states)
 		<< std::setw(10) << "S"
 		<< std::setw(10) << "I"
 		<< std::setw(10) << "R"
-		<< std::setw(10) << "R_0" << '\n';
+		<< std::setw(10) << "R" <<'\u2080' << '\n'; //should print R with subscript zero, i tried
 	int day = 1;
 	for (auto const& st : states)
 	{
@@ -90,12 +96,13 @@ void print(std::vector<State> const& states)
 int main()
 {
 	State s0{};
-	int const N = 1000000; //trial value
-	s0.S = N - 1;
-	s0.I = 1;
-	Epidemic e(s0, N);
+	int const N = 1000000; //trial value	
 	double const beta = 0.1; //trial value
 	double const gamma = 0.5; //trial value
+	s0.S = N - 1;
+	s0.I = 1;
+	s0.R_0 = s0.S * beta / gamma;
+	Epidemic e(s0, N);
 	auto spread = e.evolve(beta, gamma);
 	std::cout << std::setw(10) << " "
 		<< std::setw(10) << "N"
@@ -106,4 +113,53 @@ int main()
 		<< std::setw(10) << beta
 		<< std::setw(10) << gamma << '\n' << '\n';
 	print(spread);
+}
+
+TEST_CASE("Testing Epidemic")
+{
+	State s0{};
+	int const N = 100;
+	s0.S = N - 1;
+	s0.I = 1; //not defining R_0 bc it doesn't need testing (or does it)
+	Epidemic e(s0, N);
+
+	SUBCASE("no epidemic") {
+		double const beta = 0.;
+		double const gamma = 0.5;
+		auto spread = e.evolve(beta, gamma);
+	}
+
+	SUBCASE("no chance of recovery") {
+		double const beta = 0.5;
+		double const gamma = 0.;
+		auto spread = e.evolve(beta, gamma);
+	}
+
+	SUBCASE("beta=0.1, gamma=0.9") { //test values for beta, gamma
+		double const beta = 0.1;
+		double const gamma = 0.9;
+		auto spread = e.evolve(beta, gamma);
+		State const& s1 = spread[1];
+		CHECK(s_1.S == doctest::Approx(89.1)); //test 2nd element of spread
+		CHECK(s_1.I == doctest::Approx(10));
+		CHECK(s_1.R == doctest::Approx(0.9));
+		State const& s_last = spread.back();
+		CHECK(s_last.S == doctest::Approx(0)); //test last element of spread
+		CHECK(s_last.I == doctest::Approx(0));
+		CHECK(s_last.R == doctest::Approx(100));
+	}
+
+	SUBCASE("beta=0.4, gamma=0.1") { //test values for beta, gamma
+		double const beta = 0.4;
+		double const gamma = 0.1;
+		auto spread = e.evolve(beta, gamma);
+		State const& s1 = spread[1];
+		CHECK(s_1.S == doctest::Approx(59.4)); //test 2nd element of spread
+		CHECK(s_1.I == doctest::Approx(40.5));
+		CHECK(s_1.R == doctest::Approx(0.1));
+		State const& s_last = spread.back();
+		CHECK(s_last.S == doctest::Approx(0)); //test last element of spread
+		CHECK(s_last.I == doctest::Approx(0));
+		CHECK(s_last.R == doctest::Approx(100));
+	}
 }
