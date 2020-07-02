8e57e68aec55c821beac8945ecadc8433f089509
siberiankhatru <martinabevini@gmail.com>
2020-07-02 16:12:31 +0200

    Change definition of initial state in main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@ int main()
 	int const N = 1000000;	  //trial value
 	double const beta = 0.1;  //trial value
 	double const gamma = 0.5; //trial value
-	State s0{};
-	s0.I = 1;
-	s0.S = N - s0.I;
-	s0.R_0 = s0.S * beta / gamma;
+	State s0{ N - (s0.I + s0.R), 1., 0., s0.S * beta / gamma };
 	Epidemic e(s0, N);
 	auto spread = e.evolve(beta, gamma);
 	std::cout << std::setw(10) << " "
